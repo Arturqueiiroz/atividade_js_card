a0f80a4d037eb3b088c5ab636b7ee4efbfeee43b
ARTUR COSMO DE QUEIROZ <artur.cqueiroz@senacsp.edu.br>
2026-02-12 17:14:58 -0300

    Adicionando JavaCript e alterando o css e html

---
+++ javacript/card.js
@@ -0,0 +1,31 @@
+sobreMimBtn1.addEventListener("click", function () {
+    titulosome.classList.toggle("ola");
+    textosome.classList.toggle("ola");
+});
+
+habilidadesBtn1.addEventListener("click", function () {
+    habilidadessome.classList.toggle("ola");
+    habilidadessome1.classList.toggle("ola");
+});
+
+contatoBtn1.addEventListener("click", function () {
+    contatosome.classList.toggle("ola");
+    gitsome.classList.toggle("ola");
+    telefonesome.classList.toggle("ola");
+});
+
+sobreMimBtn2.addEventListener("click", function () {
+    titulosome1.classList.toggle("ola");
+    textosome1.classList.toggle("ola");
+});
+
+habilidadesBtn2.addEventListener("click", function () {
+    habilidadessome2.classList.toggle("ola");
+    habilidadessome3.classList.toggle("ola");
+});
+
+contatoBtn2.addEventListener("click", function () {
+    contatosome1.classList.toggle("ola");
+    telefonesome1.classList.toggle("ola");
+    gitsome1.classList.toggle("ola");
+});
